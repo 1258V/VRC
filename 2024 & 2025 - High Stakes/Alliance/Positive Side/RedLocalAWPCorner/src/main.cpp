847cd7edc9b2ed0f2763c1a401484141484bde29
Arnav Jha <overrideeducation@gmail.com>
2025-02-01 00:22:08 -0800

    why did i reset timer lol

--- 2024 & 2025 - High Stakes/Alliance/Positive Side/RedLocalAWPCorner/src/main.cpp
+++ 2024 & 2025 - High Stakes/Alliance/Positive Side/RedLocalAWPCorner/src/main.cpp
@@ -183,7 +183,6 @@ bool mobilePneu = false;
 void loadArm() {
   double ang = 29.8;
   double err = 5.6;
-  Brain.resetTimer();
   if(360-ArmRotation.angle() < (ang - err)){
     while(360-ArmRotation.angle() < (ang - err)){
       Arm.spin(reverse);
